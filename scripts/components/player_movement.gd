b7e83c699ef311736ef5635f74655ae660d39ea4
Henry Cadogan <Henry.Cadogan@gmail.com>
2024-10-28 17:11:51 +0000

    Initial ground tilemap and player movement

---
+++ scripts/components/player_movement.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed := 200.0
+
+func _physics_process(delta: float) -> void:
+	var direction := Vector2.ZERO
+
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+
+	velocity = direction.normalized() * speed
+
+	# Rotate the player to face the mouse cursor
+	look_at(get_global_mouse_position())
+
+	move_and_slide()
